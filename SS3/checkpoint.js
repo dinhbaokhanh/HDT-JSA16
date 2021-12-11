5039518ca45974e18497a8ce6af7fd4f3d0c680a
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2021-12-11 19:59:16 +0700

    commit

---
+++ SS3/checkpoint.js
@@ -0,0 +1,21 @@
+/*function findEven(number) {
+    for(i = 0; i <= number; i ++){
+        if(i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+findEven(20) */
+
+
+/*function numberOneTriangle(){
+    for (i = 1; i <= 5; i++){
+        for (j=1; j <= i; j ++){
+            document.write('1')
+        }
+        document.write("<br/>")
+    }
+    console.log(str)
+}
+numberOneTriangle() */
+
