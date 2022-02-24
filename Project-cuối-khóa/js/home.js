868ef60cbf31856ce493ecebfa2ecaf40ddd414f
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2022-02-24 09:43:37 +0700

    commit

--- Project-cuối-khóa/js/home.js
+++ Project-cuối-khóa/js/home.js
@@ -1,4 +1,29 @@
-const article = document.getElementById("thumbnail1");
-article.addEventListener("click", () => {
-    window.location.href = './news/thumbnail1.html';
+const article1 = document.getElementById("thumbnail1");
+article1.addEventListener("click", () => {
+    window.location.href = './news/new1.html';
+})
+
+const article2 = document.getElementById("thumbnail2");
+article2.addEventListener("click", () => {
+    window.location.href = './news/new2.html';
+})
+
+const article3 = document.getElementById("thumbnail3");
+article3.addEventListener("click", () => {
+    window.location.href = './news/new3.html';
+})
+
+const article4 = document.getElementById("thumbnail4");
+article4.addEventListener("click", () => {
+    window.location.href = './news/new4.html';
+})
+
+const article5 = document.getElementById("thumbnail5");
+article5.addEventListener("click", () => {
+    window.location.href = './news/new5.html';
+})
+
+const article6 = document.getElementById("thumbnail6");
+article6.addEventListener("click", () => {
+    window.location.href = './news/new6.html';
 })
