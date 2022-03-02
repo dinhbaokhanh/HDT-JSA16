efb7a313b0f6af539b47a2f697ee37f36784a070
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2022-03-03 00:20:52 +0700

    commit

--- Project/js/list.js
+++ Project/js/list.js
@@ -1,33 +1,85 @@
 function change() {
-    let results = Array.from(document.querySelectorAll('.goods > div')),
-        groupsChecked = document.querySelectorAll('.filter .groups input:checked'),
-        categoriesChecked = document.querySelectorAll('.filter .categories input:checked');
-
-
-    // Hide all results
-    results.forEach(function(result) {
-        result.style.display = 'none';
-    });
-
-
-    // Filter results to only those that meet ALL requirements:        
-    filterGroupsOrCategories(groupsChecked);
-
-    if (categoriesChecked.length != 0) {
-        filterGroupsOrCategories(categoriesChecked);
-    }
-
-    function filterGroupsOrCategories(GroupsOrCategoriesChecked) {
-        results = Array.from(GroupsOrCategoriesChecked).reduce(function(sum, input) {
-            const attrib = input.getAttribute('rel');
-            return sum.concat(results.filter(function(result) {
-                return result.classList.contains(attrib);
-            }));
-        }, []);
-    }
-    // Show those filtered results:
-    results.forEach(function(result) {
-        result.style.display = 'block';
-    });
+    var groupsCbs = document.querySelectorAll(".groups input[type='checkbox']");
+    var categoriesCbs = document.querySelectorAll(".categories input[type='checkbox']");
+    var filters = {
+        groups: getClassOfCheckedCheckboxes(groupsCbs),
+        categories: getClassOfCheckedCheckboxes(categoriesCbs)
+    };
+  
+    filterResults(filters);
 }
-change();
+  
+function getClassOfCheckedCheckboxes(checkboxes) {
+    var classes = [];
+  
+    if (checkboxes && checkboxes.length > 0) {
+      for (var i = 0; i < checkboxes.length; i++) {
+        var cb = checkboxes[i];
+  
+        if (cb.checked) {
+          classes.push(cb.getAttribute("rel"));
+        }
+      }
+    }
+  
+    return classes;
+}
+  
+function filterResults(filters) {
+    var rElems = document.querySelectorAll(".goods div");
+    var hiddenElems = [];
+  
+    if (!rElems || rElems.length <= 0) {
+        return;
+    }
+  
+    for (var i = 0; i < rElems.length; i++) {
+      var el = rElems[i];
+  
+        if (filters.groups.length > 0) {
+            var isHidden = true;
+  
+            for (var j = 0; j < filters.groups.length; j++) {
+                var filter = filters.groups[j];
+  
+                if (el.classList.contains(filter)) {
+                    isHidden = false;
+                    break;
+                }
+            }
+  
+            if (isHidden) {
+                hiddenElems.push(el);
+            }
+        }
+  
+        if (filters.categories.length > 0) {
+            var isHidden = true;
+  
+                for (var j = 0; j < filters.categories.length; j++) {
+                    var filter = filters.categories[j];
+  
+                    if (el.classList.contains(filter)) {
+                        isHidden = false;
+                        break;
+                    }
+                }
+  
+            if (isHidden) {
+                hiddenElems.push(el);
+            }
+        }
+    }
+  
+    for (var i = 0; i < rElems.length; i++) {
+        rElems[i].style.display = "block";
+    }
+  
+    if (hiddenElems.length <= 0) {
+        return;
+    }
+  
+    for (var i = 0; i < hiddenElems.length; i++) {
+        hiddenElems[i].style.display = "none";
+    }
+}
