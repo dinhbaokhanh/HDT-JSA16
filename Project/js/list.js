07a3cded1439c4b967fb1469ddb0c4d1785d9b59
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2022-03-03 16:03:42 +0700

    commit

--- Project/js/list.js
+++ Project/js/list.js
@@ -1,3 +1,5 @@
+// filter
+
 function change() {
     var groupsCbs = document.querySelectorAll(".groups input[type='checkbox']");
     var categoriesCbs = document.querySelectorAll(".categories input[type='checkbox']");
@@ -81,5 +83,45 @@ function filterResults(filters) {
   
     for (var i = 0; i < hiddenElems.length; i++) {
         hiddenElems[i].style.display = "none";
+    }
+}
+
+// search
+
+// Bản rút gọn
+// function SearchEngine() {
+//     var input = document.getElementById("search-input");
+//     var filter = input.value.toLowerCase();
+//     var items = document.getElementsByClassName('items');
+//     var items_name = document.getElementsByClassName('items-name')
+  
+//     for (i = 0; i < items_name.length; i++) {
+//       if (items_name[i].innerText.toLowerCase().includes(filter)) {
+//         items_name[i].style.display = "block";
+//       } else {
+//         items_name[i].style.display = "none";
+//       }
+//     }
+// }
+
+
+
+function SearchEngine() {
+    // Lấy giá trị
+    var input, filter, goods, items, items_name, i, txtValue;
+    input = document.getElementById('search-input');
+    filter = input.value.toLowerCase();
+    goods = document.getElementsByClassName('goods');
+    items = document.getElementsByClassName('items');
+  
+    // Ẩn div ko có trong search
+    for (i = 0; i < items.length; i++) {
+        items_name = items[i].getElementsByClassName('items-name')[0];
+        txtValue = items_name.textContent || items_name.innerText;
+        if (txtValue.toLowerCase().indexOf(filter) > -1) {
+            items[i].style.display = "";
+        } else {
+            items[i].style.display = "none";
+        }
     }
 }
