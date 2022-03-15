11ba64d45302d2cec1c0995c124a102cac261ac2
dinhbaokhanh <64970155+dinhbaokhanh@users.noreply.github.com>
2022-03-15 22:05:14 +0700

    commit

--- Project/js/SignUp.js
+++ Project/js/SignUp.js
@@ -2,7 +2,7 @@ let users = [];
 
 // Nen co dong nay.
 if (localStorage.getItem("users") == null) {
-    localStorage.setItem("users", JSON.stringify(users))
+  localStorage.setItem("users", JSON.stringify(users))
 }
 
 
@@ -12,23 +12,22 @@ if (localStorage.getItem("users") == null) {
 
 const signUp = document.getElementById("sign-up");
 signUp.addEventListener("submit", (event) => {
-    event.preventDefault();
-    // dung de chan su kien reload mac dinh cua trinh duyet
-      const data = {
-          email: signUp.email.value,
-          password: signUp.password.value
-      }
-      console.log(data);
-    // LAY DL tren localStorage ve
-      let getUsersOnLocalStorage = JSON.parse(localStorage.getItem("users"));
-      getUsersOnLocalStorage.push(data);
-      localStorage.setItem('users', JSON.stringify(getUsersOnLocalStorage))
+  event.preventDefault();
+  // dung de chan su kien reload mac dinh cua trinh duyet
+  const data = {
+    email: signUp.email.value,
+    password: signUp.password.value
+  }
+  console.log(data);
+  // LAY DL tren localStorage ve
+  let getUsersOnLocalStorage = JSON.parse(localStorage.getItem("users"));
+  getUsersOnLocalStorage.push(data);
+  localStorage.setItem('users', JSON.stringify(getUsersOnLocalStorage))
 
-      signUp.email.value = ""
-      signUp.password.value = ""
+  signUp.email.value = ""
+  signUp.password.value = ""
 
 
-    // windows.location.href = '../signIn.html'
-      window.location.href = "./LogIn.html";
+  // windows.location.href = '../signIn.html'
+  window.location = "./LogIn.html";
 })
-
